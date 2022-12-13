87ec035a76eae57ffec448d016361aad4d7db0b4
vssandip <sandipshindesr@gmail.com>
2022-12-13 12:20:28 +0530

    added: workshoparrays.sh add

---
+++ workshoparrays.sh
@@ -0,0 +1,29 @@
+couter=0
+number[((counter++))]="0"
+number[((counter++))]="1"
+number[((counter++))]="2"
+number[((counter++))]="3"
+number[((counter++))]="4"
+number[((counter++))]="5"
+number[((counter++))]="6"
+number[((counter++))]="7"
+number[((counter++))]="8"
+number[((counter++))]="9"
+number[((counter++))]="10"
+number[((counter++))]="11"
+number[((counter++))]="12"
+number[((counter++))]="13"
+number[((counter++))]="14"
+number[((counter++))]="15"
+number[((counter++))]="16"
+number[((counter++))]="17"
+number[((counter++))]="18"
+number[((counter++))]="19"
+number[((counter++))]="20"
+
+echo ${number[@]}
+echo ${arr[@]:1}
+echo ${arr[@]:10}
+echo ${arr[@]:15}
+
+
